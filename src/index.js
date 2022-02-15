32d9852bc5e66cacf2b047ea322d0a1844861fa4
Mateo Palacio <92959799+Mattheons@users.noreply.github.com>
2022-02-15 10:49:51 -0500

    act
    act

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoutes = require("./routes/users")
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+//middleware
+app.use(express.json);
+app.use('/api', userRoutes);
+
+//rutas
+app.get("/", (req, res) => {
+    res.send("Bienvenue")
+});
+
+//mongodb conect
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Conectado a ATLAS"))
+    .catch((error) => console.error(error));
+
+app.listen(port, () => console.log('server listening on port', port))
